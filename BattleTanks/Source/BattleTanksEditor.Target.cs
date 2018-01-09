96cef074257ae922a1e04bca43e9490889983b60
dennis-zinzi <dennis_zinzi@live.com>
2018-01-09 16:31:25 +0100

    Added UE4 files + made landscape

---
+++ BattleTanks/Source/BattleTanksEditor.Target.cs
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BattleTanksEditorTarget : TargetRules
+{
+	public BattleTanksEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+        //Enable IWYU for v4.17+
+        bUseUnityBuild = false;
+        bUsePCHFiles = false;
+
+        ExtraModuleNames.AddRange( new string[] { "BattleTanks" } );
+	}
+}
